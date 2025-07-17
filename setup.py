751239aa326240e65e6c622f75a90600e3fd4cbb
Kevin Santo Cappuccio <KevinC@ppucc.io>
2025-07-16 20:03:28 -0700

    "works on my machine"

--- setup.py
+++ setup.py
@@ -10,10 +10,15 @@ setup(
     description="Jumperless documentation with custom Python lexer",
     packages=find_packages(),
     install_requires=[
-        "mkdocs>=1.4.0",
-        "pymdown-extensions>=9.0",
         "pygments>=2.12.0",
     ],
+    extras_require={
+        "docs": [
+            "mkdocs>=1.4.0",
+            "pymdown-extensions>=9.0",
+            "pygments>=2.12.0",
+        ],
+    },
     entry_points={
         'pygments.lexers': [
             'jumperless = docs.lexers.jumperless_lexer:JumperlessPythonLexer',
